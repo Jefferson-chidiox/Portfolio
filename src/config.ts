54ebcaabbac54d6618b38b16f0f83f196d636105
Jefferson-chidiox <Jeffersonchidiox@gmail.com>
2024-07-16 00:33:42 +0100

    Completed Homepage

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Jefferson Nwokoro | Software Developer';
+export const SITE_DESCRIPTION = 'Let me build your dream website for you, no matter how crazy your vision may sound, I will make it happen';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
